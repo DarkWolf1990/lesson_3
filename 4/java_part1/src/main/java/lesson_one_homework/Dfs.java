98afae0e6077f0ac87885d181edc0d1fb8038d9c
YOUR_NAME <YOUR_EMAIL>
2023-01-15 18:50:03 +0300

    task Dfs

---
+++ 4/java_part1/src/main/java/lesson_one_homework/Dfs.java
@@ -0,0 +1,21 @@
+package lesson_one_homework;
+
+public class Dfs {
+    public static void main(String[] args) {
+        dfs(1, 7,2,1, "");
+    }
+
+    // dfs - обход в глубину
+    private static void dfs(int src, int target, int mult, int add, String path) {
+        if (src == target) {
+            System.out.println(path);
+            return;
+        }
+        if (src > target) {
+            return;
+        }
+        dfs(src * mult, target, mult, add, path + "k1");
+        dfs(src + add, target, mult, add, path + "k2");
+
+    }
+}
