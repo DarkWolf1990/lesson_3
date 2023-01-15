3214a7f948330b9c1968d5c3d1550989c5f9b80a
YOUR_NAME <YOUR_EMAIL>
2023-01-15 11:18:01 +0300

    task 3

---
+++ 4/java_part1/src/main/java/lesson_one_homework/MergeStringsAlternately.java
@@ -0,0 +1,25 @@
+package lesson_one_homework;
+
+// You are given two strings word1 and word2.
+// Merge the strings by adding letters in alternating order, starting with word1.
+// if a string is longer than the other, append the additional letters onto the end
+// of the merged string.
+// Return thе merged string.  1:32:53
+public class MergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2){
+        // классы пишутся всегда с большой буквы
+        // Здесь вызывается у класса Math метод max
+        int maxLength = Math.max(word1.length(), word2.length());
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < maxLength; i++){
+            if (i < word1.length()){
+                ans.append(word1.charAt(i));
+            }
+            if (i < word2.length()){
+                ans.append(word2.charAt(i));
+            }
+        }
+        return ans.toString();
+        }
+    }
+
